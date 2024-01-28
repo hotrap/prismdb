39ff2c7b95a9bb1eb8e44f54dbc3e3566f46c84b
yfzcsc <yfzcsc@gmail.com>
2024-01-28 22:13:27 +0800

    change number of partitions

--- db/db_impl.h
+++ db/db_impl.h
@@ -357,7 +357,7 @@ class DBImpl : public DB {
   bool load_phase_ = true;
   uint64_t numKeys = 200000000;
   uint64_t numWriteKeys = numKeys; // For twitter
-  uint64_t numPartitions = 8;
+  uint64_t numPartitions = 16;
   uint64_t maxDbSizeBytes = 0;
   uint32_t maxKeySizeBytes = 8;
   uint32_t maxKVSizeBytes = 1024;
